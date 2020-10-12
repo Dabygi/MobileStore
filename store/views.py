7cf7fe25b7475e44954a5052acb0f13c7df30e78
Artem <dabygi2010@gmail.com>
2020-10-13 00:01:57 +0300

    commit 11 (шаблоны админки, категории бокового меню)

--- store/views.py
+++ store/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 
-from .models import Notebook, Smartphone
+from .models import Notebook, Smartphone, Category
 
 
 def test_view(request):
-    return render(request, 'index.html', {})
+    categories = Category.objects.get_categories_for_left_sidebar()
+    return render(request, 'index.html', {'categories': categories})
 
 class ProductDetailView(DetailView):   # Вывод информации о продукции Model-View-Template
 
