af531ed50c71bab8cbeacb934946813e16f90b4a
Artem <dabygi2010@gmail.com>
2020-10-18 19:03:37 +0300

    commit 12 (миксины)

--- store/views.py
+++ store/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
 
 from .models import Notebook, Smartphone, Category
+from .mixins import CategoryDetailMixin
 
 
-def test_view(request):
-    categories = Category.objects.get_categories_for_left_sidebar()
-    return render(request, 'index.html', {'categories': categories})
+class BaseView(View):
 
-class ProductDetailView(DetailView):   # Вывод информации о продукции Model-View-Template
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        return render(request, 'index.html', {'categories': categories})
+
+
+class ProductDetailView(CategoryDetailMixin, DetailView):   # Вывод информации о продукции Model-View-Template
 
     CT_MODEL_CLASS = {
         'notebook': Notebook,
@@ -25,7 +29,7 @@ class ProductDetailView(DetailView):   # Вывод информации о пр
     slug_url_kwarg = 'slug'                    # Model-View-Template
 
 
-class CategoryDetailView(DetailView):
+class CategoryDetailView(CategoryDetailMixin, DetailView):
 
     model = Category
     queryset = Category.objects.all()
