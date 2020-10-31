eb1fe21cacad04c38145ce6e398d935f85a0fea4
Artem <dabygi2010@gmail.com>
2020-10-31 17:00:01 +0300

    commit 22 (добавил модель заказа)

--- store/models.py
+++ store/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.urls import reverse
-
+from django.utils import timezone
 
 User = get_user_model()
 
@@ -12,7 +12,7 @@ def get_models_for_count(*model_names):
     return [models.Count(model_name) for model_name in model_names]
 
 
-def get_product_url(obj, viewname):             # Model-View-Template
+def get_product_url(obj, viewname):  # Model-View-Template
     ct_model = obj.__class__._meta.model_name
     return reverse(viewname, kwargs={'ct_model': ct_model, 'slug': obj.slug})
 
@@ -22,12 +22,12 @@ class LatestProductsManager:
 
     @staticmethod
     def get_products_for_main_page(*args, **kwargs):
-        with_respect_to = kwargs.get('with_respect_to')         # если хотим выводить приоритетные модели первыми
-        products = []                                           # финальный список товаров
+        with_respect_to = kwargs.get('with_respect_to')  # если хотим выводить приоритетные модели первыми
+        products = []  # финальный список товаров
         ct_models = ContentType.objects.filter(model__in=args)  # запрос
-        for ct_model in ct_models:                              # итерируясь вызовем свойства модели
+        for ct_model in ct_models:  # итерируясь вызовем свойства модели
             model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:6]
-            products.extend(model_products)                     # соберём модели в список
+            products.extend(model_products)  # соберём модели в список
         if with_respect_to:
             ct_model = ContentType.objects.filter(model=with_respect_to)
             if ct_model.exists():
@@ -35,16 +35,14 @@ class LatestProductsManager:
                     return sorted(
                         products, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to), reverse=True
                     )
-        return products                                         # вернём список 6-и выводимых моделей
+        return products  # вернём список 6-и выводимых моделей
 
 
 class LatestProducts:
-
     objects = LatestProductsManager()
 
 
 class CategoryManager(models.Manager):
-
     CATEGORY_NAME_COUNT_NAME = {
         'Ноутбуки': 'notebook__count',
         'Смартфоны': 'smartphone__count'
@@ -61,7 +59,6 @@ class CategoryManager(models.Manager):
             for c in qs
         ]
         return data
-
 
 
 class Category(models.Model):
@@ -94,7 +91,7 @@ class Product(models.Model):
         return self.title
 
     class Meta:
-        abstract = True                 # абстрактная модель не будет создавать миграций
+        abstract = True  # абстрактная модель не будет создавать миграций
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
 
@@ -149,8 +146,8 @@ class CartProduct(models.Model):
     """Продукт в корзине"""
     user = models.ForeignKey('Customer', verbose_name="Покупатель", on_delete=models.CASCADE)
     cart = models.ForeignKey('Cart', verbose_name="Корзина", on_delete=models.CASCADE, related_name='related_products')
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE) # Покажет все существующие модели в админке
-    object_id = models.PositiveIntegerField()                               # Идентифицирует категории
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)  # Покажет все существующие модели в админке
+    object_id = models.PositiveIntegerField()  # Идентифицирует категории
     content_object = GenericForeignKey('content_type', 'object_id')
     qty = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField("Общая цена", max_digits=9, decimal_places=2)
@@ -194,17 +191,67 @@ class Cart(models.Model):
 
 
 class Customer(models.Model):
-    """Пользователь"""
-    user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
+    """Покупатель (пользователь)"""
+    user = models.ForeignKey(User, verbose_name="Покупатель", on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, verbose_name="Номер телефона", null=True, blank=True)
     address = models.CharField(max_length=255, verbose_name="Адрес", null=True, blank=True)
+    order = models.ManyToManyField('Order', verbose_name='Заказы покупателя', related_name='related_customer')
 
     def __str__(self):
         return "Покупатель: {} {}".format(self.user.first_name, self.user.last_name)
 
     class Meta:
-        verbose_name = "Пользователь"
-        verbose_name_plural = "Пользователи"
+        verbose_name = "Покупатель"
+        verbose_name_plural = "Покупатели"
 
 
+class Order(models.Model):
+    """Модель заказа"""
 
+    STATUS_NEW = 'new'
+    STATUS_IN_PROGRESS = 'in_progress'
+    STATUS_READY = 'is_ready'
+    STATUS_COMPLETED = 'complited'
+
+    BUYING_TYPE_SELF = 'self'
+    BUYING_TYPE_DELIVERY = 'delivery'
+
+    STATUS_CHOICES = (
+        (STATUS_NEW, 'Новый заказ'),
+        (STATUS_IN_PROGRESS, 'Заказ в обработке'),
+        (STATUS_READY, 'Заказ готов'),
+        (STATUS_COMPLETED, 'Заказ выполнен'),
+    )
+
+    BUYING_TYPE_CHOICES = (
+        (BUYING_TYPE_SELF, 'Самовывоз'),
+        (BUYING_TYPE_DELIVERY, 'Доставка'),
+    )
+
+    customer = models.ForeignKey(Customer, verbose_name='Покупатель', related_name='related_orders', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255, verbose_name='Имя')
+    last_name = models.CharField(max_length=255, verbose_name='Фамилия')
+    phone = models.CharField(max_length=20, verbose_name='Телефон')
+    address = models.CharField(max_length=1024, verbose_name='Адрес', null=True, blank=True)
+    status = models.CharField(
+        max_length=100,
+        verbose_name='Статус заказа',
+        choices=STATUS_CHOICES,
+        default=STATUS_NEW,
+    )
+    buying_type = models.CharField(
+        max_length=100,
+        verbose_name='Статус заказа',
+        choices=STATUS_CHOICES,
+        default=STATUS_NEW,
+    )
+    comment = models.TextField(verbose_name='Комментарий к заказу', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now=True, verbose_name='Дата создания заказа')
+    order_date = models.DateField(verbose_name='Дата получения заказа', default=timezone.now)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
