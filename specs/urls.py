caab3a601d4fc8dbc5b58056b2460848936b8745
Artem <dabygi2010@gmail.com>
2021-02-09 21:03:09 +0300

    commit 34 (форма создания новой характеристики)

--- specs/urls.py
+++ specs/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import (
     BaseSpecView,
     NewCategoryView,
+    CreateNewFeature,
 )
 
 urlpatterns = [
     path('', BaseSpecView.as_view(), name='base-spec'),
     path('new-category/', NewCategoryView.as_view(), name='new-category'),
+    path('new_feature/', CreateNewFeature.as_view(), name='new_feature'),
 ]
--- specs/views.py
+++ specs/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 
-from .forms import NewCategoryForm
+from .forms import NewCategoryForm, NewCategoryFeatureKeyForm
 
 
 class BaseSpecView(View):
@@ -26,3 +26,23 @@ class NewCategoryView(View):
             form.save()
             return HttpResponseRedirect('/product-specs/')
         return render(request, 'new_category.html', context)
+
+
+class CreateNewFeature(View):
+
+    def get(self, request, *args, **kwargs):
+        form = NewCategoryFeatureKeyForm(request.POST or None)
+        context = {'form': form}
+        return render(request, 'new_feature.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = NewCategoryFeatureKeyForm(request.POST or None)
+        if form.is_valid():
+            new_category_feature_key = form.save(commit=False)
+            new_category_feature_key.category = form.cleaned_data['category']
+            new_category_feature_key.feature_name = form.cleaned_data['feature_name']
+            new_category_feature_key.save()
+            return HttpResponseRedirect('/product-specs/')
+        context = {'form': form}
+        return render(request, 'new_feature.html', context)
+
