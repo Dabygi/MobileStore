56b5849cce7625a970d6d97270a32154c4f0d19c
Artem Gluhotorenko <dabygi2010@gmail.com>
2021-12-16 00:02:05 +0300

    commit 45 (файл amazon s3)

--- djangoMobileStore/s3utils.py
+++ djangoMobileStore/s3utils.py
@@ -5,10 +5,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
     bucket_name = 'mobile-my-store'
-    custom_domain = '{}.s3.amazonaws.com'.format(bucket_name)
     location = 'media'
 
 class StaticStorage(S3Boto3Storage):
     bucket_name = 'mobile-my-store'
-    custom_domain = '{}.s3.amazonaws.com'.format(bucket_name)
     location = 'static'
