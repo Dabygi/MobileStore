8d6c8652fb2a954d8824b0c6887c81f6da05fee0
Artem Gluhotorenko <dabygi2010@gmail.com>
2021-12-23 00:47:28 +0300

    commit 45 (файл amazon s3)

--- djangoMobileStore/settings.py
+++ djangoMobileStore/settings.py
@@ -253,7 +253,7 @@ if USE_S3:
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = 'us-east-1'
     # AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'     #f's3.{AWS_S3_REGION_NAME}.s3.amazonaws.com'   https://mobile-my-store.s3.amazonaws.com/media/
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'     #f's3.{AWS_S3_REGION_NAME}.s3.amazonaws.com'   https://mobile-my-store.s3.us-east-1.amazonaws.com/media/
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # s3 static settings
     STATIC_LOCATION = 'static'
