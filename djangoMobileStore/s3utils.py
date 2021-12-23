a9b6b3a8930ec1b6185244ac6c4d51ac2d864cb1
Artem Gluhotorenko <59452907+Dabygi@users.noreply.github.com>
2021-12-23 17:35:28 +0300

    Update s3utils.py

--- djangoMobileStore/s3utils.py
+++ djangoMobileStore/s3utils.py
@@ -1,16 +1,6 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
-# StaticRootS3BotoStorage = lambda: S3Boto3Storage(location='static')
-# MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='media')
-
-# class StaticStorage(S3Boto3Storage):
-#     location = settings.AWS_STATIC_LOCATION
-#
-# class MediaStorage(S3Boto3Storage):
-#     location = settings.AWS_PUBLIC_MEDIA_LOCATION
-#     file_overwrite = False
-
 
 class StaticStorage(S3Boto3Storage):
     location = 'static'
